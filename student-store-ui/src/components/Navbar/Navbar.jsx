828e5821a637410f513020833ac40e2bf74a833f
WinsonChen114 <14winsonchen@gmail.com>
2022-06-16 16:28:44 -0400

    Added some more necessary folders. Started creating Navbar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import "./Navbar.css"
+import { Link, } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <Link to={"/"}>Home</Link>
     </nav>
   )
 }
