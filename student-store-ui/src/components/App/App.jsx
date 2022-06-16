b06ed5607603322a86ea9a2f94bc132a5d72c567
WinsonChen114 <14winsonchen@gmail.com>
2022-06-16 13:24:53 -0400

    Actually added variables this time

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -2,22 +2,36 @@ import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
-import {BrowserRouter, Routes, Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import "./App.css"
 
 export default function App() {
+  //Array of products
+  let products = []
+  //Is API currently fetching products?
+  let isFetching = false
+  //Holds an error from API
+  let error =""
+  //Is Sidebar open?
+  let isOpen = false
+  //Shopping cart information
+  const [shoppingCart, setShoppingCart] = React.useState([])
+  //Not sure how this will work right now
+  let checkoutForm = ""
   return (
     <div className="app">
       <BrowserRouter>
         <main>
           <Routes>
-            
+
             {/*Renders Home, Navbar, and Sidebar at every path*/}
-            <Route path="/" element = {<> <Home />
-                                          <Navbar />
-                                          <Sidebar/> </>}/>
-            <Route path="/products/:productId" element={<ProductDetail/>}/>
-            
+            <Route path="/" element=
+              {<> <Home />
+                <Navbar />
+                <Sidebar /> </>} />
+            <Route path="/products/:productId" element={<ProductDetail />} />
+            <Route path="*" element={<NotFound />} />
+
             {/* <Navbar />
             <Sidebar />
             <Home /> */}
