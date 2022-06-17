4125048a50ab9eaaab892c38274c1a046dbe659e
WinsonChen114 <14winsonchen@gmail.com>
2022-06-17 16:27:54 -0400

    Added About section

---
+++ student-store-ui/src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import "./About.css"
+
+export default function About() {
+    return (
+        <div className="about">
+            <div className="content">
+                <div className="text">
+                    <p>About stuff will go here eventually I guess, I just need to fill up space right now</p>
+                </div>
+                <div>
+                    <img className="about-image" src="http://localhost:3000/src/assets/Wave_logo.png" alt="Wave logo"/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -2,19 +2,23 @@ import * as React from "react"
 import "./Home.css"
 import Hero from "../Hero/Hero"
 import ProductGrid from "../ProductGrid/ProductGrid"
+import About from "../About/About"
 
 export default function Home({products, handleAddItemToCart, handleRemoveItemFromCart, shoppingCart}) {
   return (
     <div className="home">
       {/*<Hero />*/}
+      <a id="Buylink"></a>
       <div id ="Buy">
         <ProductGrid products={products} handleAddItemToCart={handleAddItemToCart} handleRemoveItemFromCart={handleRemoveItemFromCart} 
       shoppingCart={shoppingCart}/>
       </div>
+      <a id="Aboutlink"></a>
       <div id ="About">
         <h3>About</h3>
-
+        <About />
       </div>
+      <a id="Contactlink"></a>
       <div id="Contact">
         <h3>Contact Us</h3>
       </div>
