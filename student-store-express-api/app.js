abf2ccb30d9a20860b6a2d9a539f5c842b650f1f
WinsonChen114 <14winsonchen@gmail.com>
2022-06-23 13:53:18 -0400

    Set up initial files and routes

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,11 @@
-// YOUR CODE HERE
+const express = require("express")
+const morgan = require("morgan")
+const store = require("./routes/store")
+
+const app = express()
+
+app.use(morgan("tiny"))
+app.use(express.json())
+app.use("/store", store)
+
+module.exports = app
